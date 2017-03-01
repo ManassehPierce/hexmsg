86bcf65ce872bbac7e2dfc38293cac43dd5332c2
Tyler Walker <twalker@erskine247.com>
2017-03-01 18:53:19 -0500

    Implemented messaging

--- server.js
+++ server.js
@@ -7,15 +7,31 @@ const http = require('http'),
 const App = express();
 const Server = http.createServer(App);
 const io = socketio(Server);
+App.set('views', './views');
+App.use(express.static('public'));
+// Add a view engine later
 
 // Sockets
-io.on('connection', () => {
-	// Do stuff
+io.on('connection', (client) => {
+	console.log("Client connected!");
+
+	client.on('join', (data) => {
+		console.log(data);
+	});
+
+	client.on('message', (data) => {
+		console.log(data);
+	});
+
+	client.on('disconnect', () => {
+		console.log("Client disconnected!");
+	});
+
 });
 
 // Routes
 App.get('/', (req, res) => {
-	res.send("Hello world");
+	res.sendFile(__dirname + "/views/socket.html");
 });
 
 // Server Listener
